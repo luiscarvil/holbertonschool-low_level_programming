508a71d64f7cc0113b617b01688b08a8b7a917f6
luiscarvil <1888@holbertonschool.com>
2020-06-25 23:31:56 +0000

    add string that concadenate string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,22 @@
 #include "holberton.h"
 /**
  *_strncat - function concatenates two strings.
- *
+ *@dest: pointer
+ *@src: pointer
+ *@n: int
  *Return: 0.
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int a = 0, b = 0;
+
 	while (dest[a] != '\0')
 	{
 		a++;
-	} 
-		while (b < n && src[b] != '\0')
-		{
+	}
+	while (b < n && src[b] != '\0')
+	{
 			dest[a] = src[b];
-		b++;
 		a++;
 		}
 	return (dest);
