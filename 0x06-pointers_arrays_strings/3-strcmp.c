db4aed49390fa3dc59111e23135018baee099b2c
luiscarvil <1888@holbertonschool.com>
2020-06-26 01:13:24 +0000

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,17 +13,9 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (*s1 - *s2);
 		}
-			if (*s1 != *s2)
+		if (*s1 == *s1)
 		{
-			return (*s1 - *s2);
-		}
-			if (*s1 == *s1)
-		{
-			return (0);
-		}
-		if (*s2 == *s2)
-		{
-			return (0);
+		return (0);
 		}
 		if (*s1 == '\0')
 		{
