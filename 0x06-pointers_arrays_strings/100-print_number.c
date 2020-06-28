879264142ea7c4a622f89da1b76d9278f6bf5232
luiscarvil <1888@holbertonschool.com>
2020-06-28 03:37:24 +0000

    print an integer.

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -6,31 +6,43 @@
  */
 void print_number(int n)
 {
+	unsigned int a = n;
 	if (n < 0)
 	{
-		n = n * -1;
 		_putchar('-');
-	}
-	if (n >= 0 && n <= 9)
+		a = -a;
+	} 
+	if ((a / 10) > 0)
 	{
-		_putchar(n + '0');
+		print_number(a / 10);
 	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n > 99 && n <= 999)
-	{
-		_putchar((n / 100) + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n > 999 && n <= 9999)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar((n / 100) % 10 + '0');
-	_putchar((n / 10) % 10 + '0');
-	_putchar((n % 10) + '0');
-	}
+	_putchar((a % 10) + '0');
 }
+
+/*	if (n < 0)
+*	{
+*		_putchar('-');
+*		}
+*	if (n >= 0 && n <= 9)
+*	{
+*		_putchar(n + '0');
+*	}
+*	else if (n > 9 && n <= 99)
+*	{
+*		_putchar((n / 10) + '0');
+*		_putchar((n % 10) + '0');
+*	}
+*	else if (n > 99 && n <= 999)
+*	{
+*		_putchar((n / 100) + '0');
+*		_putchar((n / 10) % 10 + '0');
+*		_putchar((n % 10) + '0');
+*	}
+*	else if (n > 999 && n <= 9999)
+*	{
+*		_putchar((n / 1000) + '0');
+*		_putchar((n / 100) % 10 + '0');
+*	_putchar((n / 10) % 10 + '0');
+*	_putchar((n % 10) + '0');
+*	}
+}*/
