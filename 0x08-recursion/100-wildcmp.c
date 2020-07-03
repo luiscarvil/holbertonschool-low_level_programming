126342426505c5f04dcd67920907f403af2674be
luiscarvil <1888@holbertonschool.com>
2020-07-03 18:24:41 +0000

    indentical strings

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -17,7 +17,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s2 == '*')
 	{
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2) || wildcmp(s1 + 1, s2 + 1))
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 		{
 			return (1);
 		}
