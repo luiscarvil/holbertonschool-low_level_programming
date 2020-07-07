95361d351d1fbb10362e2a4e6aea048d972b32f6
luiscarvil <1888@holbertonschool.com>
2020-07-07 21:40:53 +0000

    first malloc array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *create_array - array of chars with malloc
+ *@size: size of array.
+ *@c: char inicialized.
+ *Return: array s or null.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i = 0;
+
+	s = malloc(size * sizeof(c));
+	while (i < size)
+	{
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	s[i] = c;
+	i++;
+	}
+	return (s);
+}
