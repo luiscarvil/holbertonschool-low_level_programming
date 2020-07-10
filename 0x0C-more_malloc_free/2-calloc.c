4498642365861f03cb473a2412d0844a56cc70f1
luiscarvil <1888@holbertonschool.com>
2020-07-10 03:14:00 +0000

    allicates memory calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *_calloc - allocates memory for an array
+ *@nmemb: integer
+ *@size: integer size.
+ *Return: NULL or string.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		arr = malloc(nmemb * size);
+		if (arr == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < (nmemb * size); i++)
+		{
+			arr[i] = 0;
+
+		}
+		return (arr);
+	}
+}
