dd2f02ab56ec3f4d8f8953c20a5e31b34c5c4bf4
luiscarvil <1888@holbertonschool.com>
2020-07-15 22:05:32 +0000

    add print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
+#include "function_pointers.h"
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * print_name - function that print name
  * @name: print name.
@@ -9,6 +9,8 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
+	{
 		return;
+	}
 	f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,5 @@
 #ifndef FUNCTIONP_H
 #define FUNCTIONP_H
+#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 #endif
