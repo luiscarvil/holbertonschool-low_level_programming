233e97b6b17b882977fd4a0a36e24d560bc205a0
luiscarvil <1888@holbertonschool.com>
2020-07-17 23:55:07 +0000

    add sum them all num

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - add arguments in function.
+ * @n: num arguments unsigned int.
+ * Return 0 or total.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int total;
+	va_list valores;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(valores, n);
+	total = 0;
+	i = 0;
+	while (i < n)
+	{
+		total = total + va_arg(valores, int);
+		i++;
+	}
+	va_end(valores);
+	return (total);
+}
