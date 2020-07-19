32b03c13ca5dca71379d80754c115bcec665c634
luiscarvil <1888@holbertonschool.com>
2020-07-19 23:13:40 +0000

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,46 +1,48 @@
 #include "variadic_functions.h"
+/**
+ * print_all - print numbes, followed bu a new line.
+ * * @format: pointer num arguments.
+ * Return: nothing void.
+ */
 void print_all(const char *const format, ...)
 {
-        va_list form;
-        char *stg;
-         int i = 0;
-        char *space;
-        va_start(form, format);
-       
+	va_list form;
+	char *stg;
+	int i;
+	char *space;
 
-        while (format[i] != '\0')
-        {
-                space = "";
-                if (format[i + 1])
-                {
-                        space = ", ";
-                }
+	va_start(form, format);
+	for  (i = 0; format[i] != '\0'; i++)
+	{
+		space = "";
+		if (format[i + 1])
+		{
+			space = ", ";
+		}
 
-                switch (format[i])
-                {
-                case 'c':
-                        printf("%c%s", (char) va_arg(form, int), space);
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c%s", (char)va_arg(form, int), space);
 
-                        break;
-                case 'i':
-                int print_int(va_list i)
-                        printf("%i%s", va_arg(form, int), space);
-                        break;
-                case 'f':
-                        printf("%f%s", (float)va_arg(form, int), space);
-                        break;
-                case 's':
-                        stg = va_arg(form, char *);
-                        if (stg == NULL)
-                        {
-                                printf("(nil)");
-                                break;
-                        }
-                        printf("%s%s", stg, space);
-                        break;
-                }
-                i++;
-        }
-        printf("\n");
-        va_end(form);
+			break;
+		case 'i':
+			    printf("%i%s", va_arg(form, int), space);
+			break;
+		case 'f':
+			printf("%f%s", (float)va_arg(form, int), space);
+			break;
+		case 's':
+			stg = va_arg(form, char *);
+			if (stg == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s%s", stg, space);
+			break;
+		}
+	}
+	printf("\n");
+	va_end(form);
 }
