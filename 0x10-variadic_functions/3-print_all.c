060c051a3ee6a8bca5e21c26849753ce3539084a
luiscarvil <1888@holbertonschool.com>
2020-07-20 00:42:56 +0000

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -43,9 +43,9 @@ void print_all(const char *const format, ...)
 				space = ", ";
 				printf("%s", va_arg(form, char *));
 				break;
-			}			i++;
+			}
+			i++;
 		}
 	}
 	printf("\n");
-	va_end(form);
 }
