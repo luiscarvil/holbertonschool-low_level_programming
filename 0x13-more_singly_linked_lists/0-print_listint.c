a3bc0e1b100a6018d6e4762a2ca9cbf46c722e0c
luiscarvil <1888@holbertonschool.com>
2020-07-28 23:06:13 +0000

    list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,19 @@
 #include "lists.h"
+/**
+ *print_listint - print list int.
+ *@h: pointer structure.
+ *Return: lista.
+ */
 size_t print_listint(const listint_t *h)
 {
-	size_t lista= NULL;
+	size_t lista = NULL;
+
 	while (h != NULL)
 	{
 	printf("%d\n", h->n);
 	lista++;
 	h = h->next;
 	}
-	return(lista);
-	
+	return (lista);
 }
+
