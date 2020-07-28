1bbb2c9cd2f7148f85fc0d37d3116669ce4e46e8
luiscarvil <1888@holbertonschool.com>
2020-07-28 23:23:23 +0000

    node int end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,8 +1,14 @@
 #include "lists.h"
-
+/**
+ *add_nodeint_end - add node end.
+ *@head: pointer double.
+ *@n: int const.
+ *Return: NULL or *head.
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *lista, *temp;
+
 	temp = NULL;
 	lista = malloc(sizeof(listint_t));
 	if (lista == NULL)
@@ -22,6 +28,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		temp = temp->next;
 	}
 	temp->next = lista;
-
 	return (*head);
 }
