704fc034ca38cd3e2dffe788d93b46373a8acad9
luiscarvil <1888@holbertonschool.com>
2020-07-28 23:41:25 +0000

    free list 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,13 @@
 #include "lists.h"
+/**
+ *free_listint2 - free list.
+ *@head: double pointer.
+ *Return: nothing.
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *lista;
-	
+
 	if (head != NULL)
 	{
 		while (*head != '\0')
