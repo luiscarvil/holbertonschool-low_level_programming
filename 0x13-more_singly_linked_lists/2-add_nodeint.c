d38042b4fa36df884a1ffa1f7aa2308175e5b0f8
luiscarvil <1888@holbertonschool.com>
2020-07-28 23:19:41 +0000

    new node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,14 +1,21 @@
 #include "lists.h"
+/**
+ *add_nodeint - add new node begin.
+ *@head: pointer double.
+ *@n: int const.
+ *Return: lista or null.
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *lista;
+
 	lista = malloc(sizeof(listint_t));
 	if (lista == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	lista->n = n;
 	lista->next = *head;
 	*head = lista;
-	return(lista);
+	return (lista);
 }
