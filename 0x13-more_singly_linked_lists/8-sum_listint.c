d02c5615d592ac24d76c778c7f496fc09191d442
luiscarvil <1888@holbertonschool.com>
2020-07-30 10:32:22 -0500

    sum all data (n) of a listint_t

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_listint - sum all the data(n) of a listint_t linked list.
+ * @head: pointer struc.
+ * Return total (data(n)).
+ */
+int sum_listint(listint_t *head)
+{
+	int total;
+
+	while (head != NULL)
+	{
+		total = total + head->n;
+		head = head->next;
+	}
+	return (total);
+}
