693a8cb938462d6e834144187c36f0a3023f9952
luiscarvil <1888@holbertonschool.com>
2020-07-30 23:04:21 -0500

    binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,31 +8,29 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i, j;
 	unsigned int total;
+
 	i = 0;
 	if (b == NULL)
 	{
 		return (0);
 	}
-	while(b[i] != '\0')
+	while (b[i] != '\0')
 	{
 		i++;
 	}
-	
 	i--;
-	
 	j = 1;
 	total = 0;
 	while (i >= 0)
 	{
-		
 		if (b[i] != '1' && b[i] != '0')
 		{
 			return (0);
 		}
-		total +=(b[i] - 48) * j;
-		
+		total += (b[i] - 48) * j;
+
 		j *= 2;
-		i--;		
+		i--;
 	}
-	return(total);
+	return (total);
 }
