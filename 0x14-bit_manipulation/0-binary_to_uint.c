550d0a92f0cf90d5f2d0e2844af39a0a2c5656f4
luiscarvil <1888@holbertonschool.com>
2020-07-30 22:57:23 -0500

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - past num binary to decimal.
+ * @b: pointer string.
+ * Return: 0 if NULL or != 0,1 or num decimal.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, j;
+	unsigned int total;
+	i = 0;
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while(b[i] != '\0')
+	{
+		i++;
+	}
+	
+	i--;
+	
+	j = 1;
+	total = 0;
+	while (i >= 0)
+	{
+		
+		if (b[i] != '1' && b[i] != '0')
+		{
+			return (0);
+		}
+		total +=(b[i] - 48) * j;
+		
+		j *= 2;
+		i--;		
+	}
+	return(total);
+}
