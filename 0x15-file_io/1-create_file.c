a6758e824ae2cb535b006a23da07e2df47c0405c
luiscarvil <1888@holbertonschool.com>
2020-08-05 11:45:30 -0500

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,6 +20,11 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		close(arch);
+		return (1);
+	}
 	while (text_content[i])
 	{
 		i++;
