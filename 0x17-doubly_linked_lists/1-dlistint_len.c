cf923936853962d29a61264587aaf5d6ff835c05
luiscarvil <1888@holbertonschool.com>
2020-09-06 18:59:32 -0500

    add fintion return number elements in a linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * dlistint_len - return num elements in a linked list.
+ * @h: poinrter to struct dlistint_t
+ * Return: number of nodes 
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t c;
+
+	for (c = 0; h != NULL; c++)
+		h = h->next;
+	return (c);
+}
