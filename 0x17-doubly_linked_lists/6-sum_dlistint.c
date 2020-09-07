8abb3b72579eb8fa39f5df5ff62508f6c4b9a558
luiscarvil <1888@holbertonschool.com>
2020-09-06 20:42:04 -0500

    return the sum of all the data (n)

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,16 +1,17 @@
 #include "lists.h"
 /**
- * sum_dlistint - func that returns the sum of all the data (n) linked list. 
+ * sum_dlistint - func that returns the sum of all the data (n) linked list.
  * @head: head pointer list
  * Return: the sum of all the data (n)
  */
 int sum_dlistint(dlistint_t *head)
 {
 	int add = 0;
+
 	while (head != NULL)
 	{
 		add = add + head->n;
 		head = head->next;
 	}
-	return(add);
+	return (add);
 }
