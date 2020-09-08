76deeb6fd219ccf649f1b5cc0e03528bdec2c722
luiscarvil <1888@holbertonschool.com>
2020-09-07 21:08:13 -0500

    make me win script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://raw.githubusercontent.com/luiscarvil/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libhbtn.so
-export LD_PRELOAD=/tmp/libhb.so
+export LD_PRELOAD=/tmp/libhbtn.so
