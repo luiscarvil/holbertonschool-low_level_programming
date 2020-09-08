2c1e2e4c7d0301640566f49b4ca9aeb75e63d410
luiscarvil <1888@holbertonschool.com>
2020-09-07 21:56:18 -0500

    add librery

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.githubusercontent.com/luiscarvil/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/.so
+wget -P /tmp/ https://raw.githubusercontent.com/luiscarvil/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/random.so
 export LD_PRELOAD=/tmp/random.so
