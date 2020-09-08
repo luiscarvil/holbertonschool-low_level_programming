97f154b9b985962f911eefb8d3f2cb03992e5dd5
luiscarvil <1888@holbertonschool.com>
2020-09-07 21:27:49 -0500

    script LD_PRELOAD

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://raw.githubusercontent.com/luiscarvil/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libhbtn.so
+wget -P /tmp/ https://raw.githubusercontent.com/luiscarvil/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libhbtn.so
 export LD_PRELOAD=/tmp/libhbtn.so
