569354d8a5ab588c903f1c98fa8ffdf5bb5b27a3
luiscarvil <1888@holbertonschool.com>
2020-10-02 13:40:18 -0500

    add hash tables functions

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,22 +7,22 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned int i = 0;
-    hash_node_t *aux_tmp = NULL, *node = NULL;
+	unsigned int i = 0;
+	hash_node_t *aux_tmp = NULL, *node = NULL;
 
-    while (i < ht->size)
-    {
-        aux_tmp = ht->array[i];
-        while(aux_tmp != NULL)
-        {
-            node = aux_tmp->next;
-            free(aux_tmp->value);
-            free(aux_tmp->key);
-            free(aux_tmp);
-            aux_tmp = node;
-        }
-        i++;
-    }
-    free(ht->array);
-    free(ht);
+	while (i < ht->size)
+	{
+		aux_tmp = ht->array[i];
+		while (aux_tmp != NULL)
+		{
+			node = aux_tmp->next;
+			free(aux_tmp->value);
+			free(aux_tmp->key);
+			free(aux_tmp);
+			aux_tmp = node;
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
 }
