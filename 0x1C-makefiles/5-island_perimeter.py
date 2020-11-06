0f407269039d8043d22f433dc15909d9ed2bc594
luiscarvil <1888@holbertonschool.com>
2020-11-06 18:21:07 -0500

    function that return the perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,9 @@
 
 
 def island_perimeter(grid):
+    """perimeter function
+    return count perimeter
+    """
     per = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -12,7 +15,7 @@ def island_perimeter(grid):
                     per += 1
                 if j - 1 < 0 or grid[i][j-1] == 0:
                     per += 1
-                if j + 1 >= len(grid[0]) or grid[i][j+1] == 0:
+                if j + 1 >= len(grid[i]) or grid[i][j+1] == 0:
                     per += 1
                 if i + 1 >= len(grid) or grid[i + 1][j] == 0:
                     per += 1
